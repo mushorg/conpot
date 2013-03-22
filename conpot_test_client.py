dacc0bd645692b8532f43c81ef328ce1b60ca60e
glaslos <glaslos@gmail.com>
2013-03-22 14:36:48 +0100

    catching timeout

--- conpot_test_client.py
+++ conpot_test_client.py
@@ -3,23 +3,20 @@
 # (C)2009 - Apidev - http://www.apidev.fr
 # This is distributed under GNU LGPL license, see license.txt
 
+import socket
+
 import modbus_tk
 import modbus_tk.defines as cst
 import modbus_tk.modbus_tcp as modbus_tcp
 
-logger = modbus_tk.utils.create_logger("console")
 
 if __name__ == "__main__":
     try:
-        #Connect to the slave
         master = modbus_tcp.TcpMaster()
-        master.set_timeout(5.0)
-        logger.info("connected")
+        master.set_timeout(1.0)
 
-        logger.info(master.execute(5, cst.READ_HOLDING_REGISTERS, 0, 3))
-
-        #send some queries
-        logger.info(master.execute(5, cst.READ_COILS, 0, 10))
+        print master.execute(slave=5, function_code=cst.READ_HOLDING_REGISTERS, starting_address=0, quantity_of_x=3)
+        print master.execute(4, cst.READ_COILS, 0, 10)
         #logger.info(master.execute(1, cst.READ_DISCRETE_INPUTS, 0, 8))
         #logger.info(master.execute(1, cst.READ_INPUT_REGISTERS, 100, 3))
         #logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 100, 12))
@@ -29,4 +26,6 @@ if __name__ == "__main__":
         #logger.info(master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 100, output_value=xrange(12)))
 
     except modbus_tk.modbus.ModbusError, e:
-        logger.error("%s- Code=%d" % (e, e.get_exception_code()))
+        print e.get_exception_code()
+    except socket.timeout:
+        print "Timeout"
