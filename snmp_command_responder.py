47ea3f8062e8669ac7c646bccefcb1878557ffc6
glaslos <glaslos@gmail.com>
2013-04-11 13:08:58 +0200

    New SNMP command responser

--- snmp_command_responder.py
+++ snmp_command_responder.py
@@ -1,128 +1,102 @@
 # Command Responder (GET/GETNEXT)
 # Based on examples from http://pysnmp.sourceforge.net/
 
-import time
-import bisect
-
-from pyasn1.codec.ber import encoder, decoder
-from pysnmp.proto import api
-from pysnmp.entity import engine
-from pysnmp.smi import builder
+from pysnmp.entity import config
+from pysnmp.entity.rfc3413 import cmdrsp, context
+from pysnmp.carrier.asynsock.dgram import udp
 
 import gevent
 from gevent import socket
+
 from modules.udp_server import DatagramServer
+from modules import snmp_engine as engine
+
+import config as conpot_config
 
 
-class SysDescr(object):
-    name = (1, 3, 6, 1, 2, 1, 1, 1, 0)
+class SNMPDispatcher(DatagramServer):
 
-    def __cmp__(self, other):
-        return cmp(self.name, other)
-
-    def __call__(self, protoVer):
-        return api.protoModules[protoVer].OctetString('PySNMP example command responder at %s' % __file__)
-
-
-class Uptime(object):
-    name = (1, 3, 6, 1, 2, 1, 1, 3, 0)
-    birthday = time.time()
-
-    def __cmp__(self, other):
-        return cmp(self.name, other)
-
-    def __call__(self, protoVer):
-        return api.protoModules[protoVer].TimeTicks(
-            (time.time() - self.birthday) * 100)
-
-
-class SNMPServer(DatagramServer):
-
-    def _import_mib(self):
-        snmpEngine = engine.SnmpEngine()
-        mibBuilder = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder
-        mibSources = mibBuilder.getMibSources() + (
-            builder.DirMibSource('.'),
-        )
-        mibBuilder.setMibSources(*mibSources)
-
-        # Create and put on-line my managed object
-        deliveryTime, = mibBuilder.importSymbols('TRS-MIB', 'trsDeliveryTime')
-        Integer32, = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder.importSymbols('SNMPv2-SMI', 'Integer32')
-
-        MibScalarInstance, = mibBuilder.importSymbols('SNMPv2-SMI', 'MibScalarInstance')
-
-        class MyDeliveryTime(Integer32):
-
-            def readGet(self, name, val, idx, (acFun, acCtx)):
-                return name, self.syntax.clone(42)
-
-        deliveryTimeInstance = MibScalarInstance(
-            deliveryTime.name, (0,), deliveryTime.syntax
-        )
-        mibBuilder.exportSymbols('TRS-MIB', deliveryTimeInstance=deliveryTimeInstance)  # creating MIB
+    def __init__(self):
+        pass
 
     def handle(self, msg, address):
-        mibInstr = [SysDescr(), Uptime()]  # sorted by object name
-        mibInstrIdx = {}
-        for mibVar in mibInstr:
-            mibInstrIdx[mibVar.name] = mibVar
+        print repr(msg), address
 
-        while msg:
-            msg_version = api.decodeMessageVersion(msg)
-            print "msg_version", msg_version
-            try:
-                pMod = api.protoModules[msg_version]
-            except KeyError:
-                print 'Unsupported SNMP version %s' % msg_version
-                return
-            reqMsg, msg = decoder.decode(msg, asn1Spec=pMod.Message(),)
-            rspMsg = pMod.apiMessage.getResponse(reqMsg)
-            rspPDU = pMod.apiMessage.getPDU(rspMsg)
-            reqPDU = pMod.apiMessage.getPDU(reqMsg)
-            varBinds = []
-            errorIndex = -1
+    def registerTransport(self, tDomain, transport):
+        DatagramServer.__init__(self, transport, self.handle)
 
-            # GETNEXT PDU
-            if reqPDU.isSameTypeWith(pMod.GetNextRequestPDU()):
-                # Produce response var-binds
-                errorIndex = -1
-                for oid, val in pMod.apiPDU.getVarBinds(reqPDU):
-                    errorIndex += 1
-                    # Search next OID to report
-                    nextIdx = bisect.bisect(mibInstr, oid)
-                    if nextIdx == len(mibInstr):
-                        # Out of MIB
-                        pMod.apiPDU.setEndOfMibError(rspPDU, errorIndex)
-                    else:
-                        # Report value if OID is found
-                        varBinds.append(
-                            (mibInstr[nextIdx].name, mibInstr[nextIdx](msg_version))
-                        )
+    def registerRecvCbFun(self, recvCbFun):
+        print "recvCbFun", recvCbFun
 
-            elif reqPDU.isSameTypeWith(pMod.GetRequestPDU()):
-                for oid, val in pMod.apiPDU.getVarBinds(reqPDU):
-                    if not oid in mibInstrIdx:
-                        print "No such instance"
-                        pMod.apiPDU.setNoSuchInstanceError(rspPDU, errorIndex)
-                        varBinds = pMod.apiPDU.getVarBinds(reqPDU)
-                        break
+    def registerTimerCbFun(self, timerCbFun, tickInterval=None):
+        print "timerCbFun", timerCbFun
 
-                    varBinds.append((oid, mibInstrIdx[oid](msg_version)))
+    def sendMessage(self, outgoingMessage, transportDomain, transportAddress):
+        print outgoingMessage, transportDomain, transportAddress
 
-            else:
-                # Report unsupported request type
-                pMod.apiPDU.setErrorStatus(rspPDU, 'genErr')
 
-            pMod.apiPDU.setVarBinds(rspPDU, varBinds)
-            data = encoder.encode(rspMsg)
-            self.socket.sendto(data, address)
+class CommandResponder(object):
+
+    def addSocketTransport(self, snmpEngine, transportDomain, transport):
+        """Add transport object to socket dispatcher of snmpEngine"""
+        if not snmpEngine.transportDispatcher:
+            snmpEngine.registerTransportDispatcher(SNMPDispatcher())
+        snmpEngine.transportDispatcher.registerTransport(transportDomain, transport)
+
+    def __init__(self):
+        # Create SNMP engine
+        self.snmpEngine = engine.SnmpEngine()
+        # Transport setup
+
+        udp_sock = gevent.socket.socket(gevent.socket.AF_INET, gevent.socket.SOCK_DGRAM)
+        udp_sock.setsockopt(gevent.socket.SOL_SOCKET, gevent.socket.SO_BROADCAST, 1)
+        udp_sock.bind((conpot_config.snmp_host, conpot_config.snmp_port))
+        # UDP over IPv4
+        self.addSocketTransport(
+            self.snmpEngine,
+            udp.domainName,
+            udp_sock
+        )
+
+        # SNMPv3/USM setup
+
+        # user: usr-md5-des, auth: MD5, priv DES
+        config.addV3User(
+            self.snmpEngine, 'usr-md5-des',
+            config.usmHMACMD5AuthProtocol, 'authkey1',
+            config.usmDESPrivProtocol, 'privkey1'
+        )
+        # user: usr-sha-none, auth: SHA, priv NONE
+        config.addV3User(
+            self.snmpEngine, 'usr-sha-none',
+            config.usmHMACSHAAuthProtocol, 'authkey1'
+        )
+        # user: usr-sha-aes128, auth: SHA, priv AES/128
+        config.addV3User(
+            self.snmpEngine, 'usr-sha-aes128',
+            config.usmHMACSHAAuthProtocol, 'authkey1',
+            config.usmAesCfb128Protocol, 'privkey1'
+        )
+
+        # Allow full MIB access for each user at VACM
+        config.addVacmUser(self.snmpEngine, 3, 'usr-md5-des', 'authPriv',
+                           (1, 3, 6, 1, 2, 1), (1, 3, 6, 1, 2, 1))
+        config.addVacmUser(self.snmpEngine, 3, 'usr-sha-none', 'authNoPriv',
+                           (1, 3, 6, 1, 2, 1), (1, 3, 6, 1, 2, 1))
+        config.addVacmUser(self.snmpEngine, 3, 'usr-sha-aes128', 'authPriv',
+                           (1, 3, 6, 1, 2, 1), (1, 3, 6, 1, 2, 1))
+
+        # Get default SNMP context this SNMP engine serves
+        snmpContext = context.SnmpContext(self.snmpEngine)
+
+        # Register SNMP Applications at the SNMP engine for particular SNMP context
+        cmdrsp.GetCommandResponder(self.snmpEngine, snmpContext)
+        cmdrsp.SetCommandResponder(self.snmpEngine, snmpContext)
+        cmdrsp.NextCommandResponder(self.snmpEngine, snmpContext)
+        cmdrsp.BulkCommandResponder(self.snmpEngine, snmpContext)
 
 
 if __name__ == "__main__":
-    udp_sock = gevent.socket.socket(gevent.socket.AF_INET, gevent.socket.SOCK_DGRAM)
-    udp_sock.setsockopt(gevent.socket.SOL_SOCKET, gevent.socket.SO_BROADCAST, 1)
-    udp_sock.bind(('localhost', 161))
-    server = SNMPServer(udp_sock)
+    server = CommandResponder()
     print 'Starting echo server on port 161'
-    server.serve_forever()
+    server.snmpEngine.transportDispatcher.serve_forever()
