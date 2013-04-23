cecff1c80796c37efed52e75056f68ef1f8c1b1d
glaslos <glaslos@gmail.com>
2013-04-23 16:06:00 +0200

    Some SNMP client cleanings

--- tests/snmp_client.py
+++ tests/snmp_client.py
@@ -6,60 +6,61 @@ import sys
 sys.path.append('./')
 import config as conpot_config
 
-# Create SNMP engine instance
-snmpEngine = engine.SnmpEngine()
 
-#
-# SNMPv3/USM setup
-#
+class SNMPClient(object):
+    def __init__(self):
+        # Create SNMP engine instance
+        self.snmpEngine = engine.SnmpEngine()
 
-# user: usr-sha-aes, auth: SHA, priv AES
-config.addV3User(
-    snmpEngine, 'usr-sha-aes128',
-    config.usmHMACSHAAuthProtocol, 'authkey1',
-    config.usmAesCfb128Protocol, 'privkey1'
-)
-config.addTargetParams(snmpEngine, 'my-creds', 'usr-sha-aes128', 'authPriv')
-
-#
-# Setup transport endpoint and bind it with security settings yielding
-# a target name (choose one entry depending of the transport needed).
-#
-
-# UDP/IPv4
-config.addSocketTransport(
-    snmpEngine,
-    udp.domainName,
-    udp.UdpSocketTransport().openClientMode()
-)
-config.addTargetAddr(
-    snmpEngine, 'my-router',
-    udp.domainName, (conpot_config.snmp_host, conpot_config.snmp_port),
-    'my-creds'
-)
-
-
-# Error/response receiver
-def cbFun(sendRequestHandle, errorIndication, errorStatus, errorIndex, varBindTable, cbCtx):
-    if errorIndication:
-        print(errorIndication)
-    elif errorStatus:
-        print('%s at %s' % (
-            errorStatus.prettyPrint(),
-            errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
-            )
+        # user: usr-sha-aes, auth: SHA, priv AES
+        config.addV3User(
+            self.snmpEngine, 'usr-sha-aes128',
+            config.usmHMACSHAAuthProtocol, 'authkey1',
+            config.usmAesCfb128Protocol, 'privkey1'
         )
-    else:
-        for oid, val in varBindTable:
-            print('%s = %s' % (oid.prettyPrint(), val.prettyPrint()))
+        config.addTargetParams(self.snmpEngine, 'my-creds', 'usr-sha-aes128', 'authPriv')
 
-# Prepare and send a request message
-cmdgen.GetCommandGenerator().sendReq(
-    snmpEngine,
-    'my-router',
-    ( ((1,3,6,1,2,1,1,1,0), None), ),
-    cbFun
-)
+        # Setup transport endpoint and bind it with security settings yielding
+        # a target name (choose one entry depending of the transport needed).
 
-# Run I/O dispatcher which would send pending queries and process responses
-snmpEngine.transportDispatcher.runDispatcher()
+        # UDP/IPv4
+        config.addSocketTransport(
+            self.snmpEngine,
+            udp.domainName,
+            udp.UdpSocketTransport().openClientMode()
+        )
+        config.addTargetAddr(
+            self.snmpEngine, 'my-router',
+            udp.domainName, (conpot_config.snmp_host, conpot_config.snmp_port),
+            'my-creds'
+        )
+
+    # Error/response receiver
+    def cbFun(self, sendRequestHandle, errorIndication, errorStatus, errorIndex, varBindTable, cbCtx):
+        if errorIndication:
+            print(errorIndication)
+        elif errorStatus:
+            print('%s at %s' % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBindTable[-1][int(errorIndex)-1] or '?')
+            )
+        else:
+            for oid, val in varBindTable:
+                print('%s = %s' % (oid.prettyPrint(), val.prettyPrint()))
+
+    def get_command(self):
+        # Prepare and send a request message
+        cmdgen.GetCommandGenerator().sendReq(
+            self.snmpEngine,
+            'my-router',
+            (((1, 3, 6, 1, 2, 1, 1, 1, 0), None), ),
+            self.cbFun
+        )
+
+        # Run I/O dispatcher which would send pending queries and process responses
+        self.snmpEngine.transportDispatcher.runDispatcher()
+
+
+if __name__ == "__main__":
+    snmp_client = SNMPClient()
+    snmp_client.get_command()
