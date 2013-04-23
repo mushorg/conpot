af463c4eaadb03151cd3f3a7dc921455118c0d87
glaslos <glaslos@gmail.com>
2013-04-23 19:53:24 +0200

    Default public HPFriends auth key added #26. Modbus host/port settings renamed.

--- conpot_ics_server.py
+++ conpot_ics_server.py
@@ -65,7 +65,8 @@ if __name__ == "__main__":
     gevent.spawn(log_worker, log_queue)
 
     logger.setLevel(logging.DEBUG)
-    modbus_daemon = modbus_server.ModbusServer('templates/default.xml', log_queue).get_server(config.host, config.port)
+    modbus_daemon = modbus_server.ModbusServer('templates/default.xml', log_queue).get_server(config.modbus_host,
+                                                                                              config.modbus_port)
     servers.append(gevent.spawn(modbus_daemon.serve_forever))
 
     snmp_server = create_snmp_server('templates/default.xml', log_queue)
