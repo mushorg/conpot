2e71f0ac0fcb3b594e3711e12c6fee1a64be831e
glaslos <glaslos@gmail.com>
2013-05-03 16:38:58 +0200

    More messing with the setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,16 @@ from setuptools import setup, find_packages
 setup(
     name='Conpot',
     version='0.1.0',
-    packages=find_packages(exclude=['templates', 'docs']),
-    scripts = ['conpot_ics_server.py', 'config.py.dist'],
+    packages=find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'conpot = conpot.conpot_ics_server:main',
+        ]
+    },
+    package_data = {
+        '': ['*.txt', '*.rst'],
+        'conpot': ['*.xml'],
+    },
     url='http://conpot.org',
     license='GPL 2',
     author='Glastopf Project',
@@ -15,4 +23,8 @@ setup(
     long_description=open('README.md').read(),
     test_suite='nose.collector',
     install_requires=open('requirements.txt').read().splitlines(),
+    dependency_links = [
+        "git+git://github.com/rep/hpfeeds.git"
+        "git+git://github.com/glastopf/modbus-tk.git"
+    ],
 )
