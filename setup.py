cca7221a342382dbd2a6647e2766d4821715666e
glaslos <glaslos@gmail.com>
2013-05-14 15:31:00 +0200

    clean-ups and client script added to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import conpot
 
 
 def get_requirements():
-    with open('requirements.txt') as req_file_h:
+    with open("requirements.txt") as req_file_h:
         reqs = []
         for line in req_file_h.read().splitlines():
             if not line.startswith("https"):
@@ -16,21 +16,21 @@ def get_requirements():
 
 
 setup(
-    name='Conpot',
+    name="Conpot",
     version=conpot.__version__,
     packages=find_packages(exclude=["*.pyc", "config.py"]),
-    package_data = {
-        '': ['*.txt', '*.rst'],
-        'conpot': ['templates/*.xml', 'conpot.cfg', "tests/data/*"],
+    package_data={
+        "": ["*.txt", "*.rst"],
+        "conpot": ["templates/*.xml", "conpot.cfg", "tests/data/*"],
     },
-    url='http://conpot.org',
-    license='GPL 2',
-    author='Glastopf Project',
-    author_email='glastopf@public.honeynet.org',
+    url="http://conpot.org",
+    license="GPL 2",
+    author="Glastopf Project",
+    author_email="glastopf@public.honeynet.org",
     description="""Conpot is an ICS honeypot with the goal to collect intelligence about the motives
     and methods of adversaries targeting industrial control systems""",
-    long_description=open('README.rst').read(),
-    classifiers  = [
+    long_description=open("README.rst").read(),
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
@@ -38,13 +38,13 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Security",
-        ],
+    ],
     keywords="ICS SCADA honeypot",
-    scripts=['bin/conpot'],
-    test_suite='nose.collector',
+    scripts=["bin/conpot", "bin/conpot_hpf_client"],
+    test_suite="nose.collector",
     tests_require="nose",
     install_requires=get_requirements(),
-    dependency_links = [
+    dependency_links=[
         "git+https://github.com/rep/hpfeeds.git#egg=hpfeeds",
         "git+https://github.com/glastopf/modbus-tk.git#egg=modbus_tk"
     ],
