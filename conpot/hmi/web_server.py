de01c0cffae8269a65f366446d0778bd07c49424
glaslos <glaslos@gmail.com>
2013-06-18 00:09:43 +0200

    Web server added

--- conpot/hmi/web_server.py
+++ conpot/hmi/web_server.py
@@ -1,32 +1,70 @@
+import logging
+
+import gevent.monkey
+gevent.monkey.patch_all()
+
 from gevent.wsgi import WSGIServer
-import bottle
-from bottle import Bottle, static_file
-from jinja2 import Environment, FileSystemLoader
+from bottle import Bottle, static_file, request
+from jinja2 import Environment, FileSystemLoader, TemplateNotFound
+
+from conpot.snmp import snmp_client
 
 
-bottle.debug(True)
+logger = logging.getLogger()
 app = Bottle()
-template_env = Environment(loader=FileSystemLoader("./templates"))
 
 
-@app.route('/static/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='./static')
+class HTTPServer(object):
 
+    def __init__(self, www_host="0.0.0.0", www_port=8080, www_path="./www",
+                 snmp_host="127.0.0.1", snmp_port=161):
+        self.host, self.port = www_host, int(www_port)
+        self.snmp_host, self.snmp_port = snmp_host, snmp_port
+        self.template_env = Environment(loader=FileSystemLoader(www_path))
+        self._route()
 
-@app.route('/favicon.ico')
-def favicon():
-    return None
+    def _route(self):
+        app.route(['/', '/index.html', "/<path:path>"], method=["GET", "POST"], callback=self.root_page)
+        app.route('/favicon.ico', method="GET", callback=self.favicon)
+        app.route('/static/<filepath:path>', method="GET", callback=self.server_static)
 
+    def server_static(self, filepath):
+        return static_file(filepath, root='./static')
 
-@app.route(['/', '/<path:path>'], method=["POST", "GET"])
-def root_page(path=None):
-    if not path or path == "/":
-        path = "index.html"
-    template = template_env.get_template(path)
-    return template.render()
+    def favicon(self):
+        return None
+
+    def mock_callback(self, sendRequestHandle, errorIndication, errorStatus, errorIndex, varBindTable, cbCtx):
+        self.result = None
+        if errorIndication:
+            self.result = errorIndication
+        elif errorStatus:
+            self.result = errorStatus.prettyPrint()
+        else:
+            for oid, val in varBindTable:
+                self.result = val.prettyPrint()
+
+    def root_page(self, path=None):
+        logger.info(request.query_string)
+        if not path or path == "/":
+            path = "index.html"
+        client = snmp_client.SNMPClient(self.snmp_host, self.snmp_port)
+        OID = ((1, 3, 6, 1, 2, 1, 1, 1, 0), None)
+        client.get_command(OID, callback=self.mock_callback)
+        try:
+            template = self.template_env.get_template(path)
+        except TemplateNotFound:
+            template = self.template_env.get_template("index.html")
+        return template.render(id=self.result)
+
+    def run(self):
+        logger.info('HTTP server started on: {0}'.format((self.host, self.port)))
+        try:
+            WSGIServer((self.host, self.port), app).serve_forever()
+        except KeyboardInterrupt:
+            return 0
 
 
 if __name__ == '__main__':
-    http_server = WSGIServer(("0.0.0.0", 80), app)
-    http_server.serve_forever()
+    http_server = HTTPServer()
+    http_server.run()
