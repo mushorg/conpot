258df5fc9080b6ac78a15702135964c01b365013
glaslos <lrist@norman.com>
2013-11-11 21:32:07 +0100

    s7comm logging added. Minor PEP8 and exception fixes.

--- conpot/s7comm/s7.py
+++ conpot/s7comm/s7.py
@@ -58,12 +58,12 @@ class S7(object):
             return self.param_mapping[self.param][1]()
 
     def request_not_implemented(self):
-        raise ParseException(self.protocol, 'request not implemented in honeypot yet.')
+        raise ParseException('s7comm', 'request not implemented in honeypot yet.')
 
     def pack(self):
 
         if self.pdu_type not in self.pdu_mapping:
-            raise AssembleException(self.protocol, 'invalid or unsupported pdu type')
+            raise AssembleException('s7comm', 'invalid or unsupported pdu type')
         elif self.pdu_type in (2, 3):
             # type 2 and 3 feature an additional RESULT INFORMATION header
             return pack('!BBHHHHH', self.magic, self.pdu_type, self.reserved, self.request_id, self.param_length,
@@ -78,12 +78,12 @@ class S7(object):
         try:
             fixed_header = unpack('!BBHHHH', packet[:10])
         except struct.error:
-            raise ParseException(self.protocol, 'malformed fixed packet header structure')
+            raise ParseException('s7comm', 'malformed fixed packet header structure')
 
         self.magic = int(fixed_header[0])
 
         if self.magic != 0x32:
-            raise ParseException(self.protocol, 'bad magic number, expected 0x32 but got {0}.'.format(self.magic))
+            raise ParseException('s7comm', 'bad magic number, expected 0x32 but got {0}.'.format(self.magic))
 
         self.pdu_type = fixed_header[1]
         self.reserved = fixed_header[2]
@@ -107,7 +107,7 @@ class S7(object):
         try:
             self.param = unpack('!B', self.parameters[:1])[0]
         except:
-            raise ParseException(self.protocol, 'invalid packet')
+            raise ParseException('s7comm', 'invalid packet')
 
         return self
 
@@ -118,7 +118,7 @@ class S7(object):
         try:
             unpack('!BBBBBBBB', self.parameters[:8])
         except struct.error:
-            raise ParseException(self.protocol, 'malformed SSL/SZL parameter structure')
+            raise ParseException('s7comm', 'malformed SSL/SZL parameter structure')
 
         chunk = self.data
         chunk_id = 0
@@ -127,7 +127,7 @@ class S7(object):
             try:
                 ssl_chunk_header = unpack('!BBH', chunk[:4])
             except struct.error:
-                raise ParseException(self.protocol, 'malformed SSL/SZL data structure')
+                raise ParseException('s7comm', 'malformed SSL/SZL data structure')
 
             # dissect data blocks
 
@@ -164,67 +164,67 @@ class S7(object):
         # just for convenience
         current_ssl = S7.ssl_lists['W#16#xy11']
 
-        if (data_ssl_index == 1):    # 0x0001 - component identification
+        if data_ssl_index == 1:    # 0x0001 - component identification
 
             ssl_index_description = 'Component identification'
 
             ssl_resp_data = pack('!HHHHH20sHHH',
-                                 17, # 1  WORD   ( ID )
-                                 data_ssl_index, # 1  WORD   ( Index )
-                                 28, # 1  WORD   ( Length of payload after element count )
-                                 0x01, # 1  WORD   ( 1 element follows )
-                                 data_ssl_index, # 1  WORD   ( Data Index )
+                                 17,  # 1  WORD   ( ID )
+                                 data_ssl_index,  # 1  WORD   ( Index )
+                                 28,  # 1  WORD   ( Length of payload after element count )
+                                 0x01,  # 1  WORD   ( 1 element follows )
+                                 data_ssl_index,  # 1  WORD   ( Data Index )
                                  current_ssl['W#16#0001'],
                                  # 10 WORDS  ( MLFB of component: 20 bytes => 19 chars + 1 blank (0x20) )
-                                 0x0, # 1  WORD   ( RESERVED )
-                                 0x0, # 1  WORD   ( Output state of component )
+                                 0x0,  # 1  WORD   ( RESERVED )
+                                 0x0,  # 1  WORD   ( Output state of component )
                                  0x0)                       # 1  WORD   ( RESERVED )
 
             ssl_resp_head = pack('!BBH',
-                                 0xff, # 1  BYTE   ( Data Error Code. 0xFF = OK )
-                                 0x09, # 1  BYTE   ( Data Type. 0x09 = Char/String )
+                                 0xff,  # 1  BYTE   ( Data Error Code. 0xFF = OK )
+                                 0x09,  # 1  BYTE   ( Data Type. 0x09 = Char/String )
                                  len(ssl_resp_data))        # 1  WORD   ( Length of following data )
 
-        elif (data_ssl_index == 6):  # 0x0006 - hardware identification
+        elif data_ssl_index == 6:  # 0x0006 - hardware identification
 
             ssl_index_description = 'Hardware identification'
 
             ssl_resp_data = pack('!HHHHH20sHHH',
-                                 17, # 1  WORD   ( ID )
-                                 data_ssl_index, # 1  WORD   ( Index )
-                                 28, # 1  WORD   ( Length of payload after element count )
-                                 0x01, # 1  WORD   ( 1 element follows )
-                                 data_ssl_index, # 1  WORD   ( Data Index )
+                                 17,  # 1  WORD   ( ID )
+                                 data_ssl_index,  # 1  WORD   ( Index )
+                                 28,  # 1  WORD   ( Length of payload after element count )
+                                 0x01,  # 1  WORD   ( 1 element follows )
+                                 data_ssl_index,  # 1  WORD   ( Data Index )
                                  current_ssl['W#16#0006'],
                                  # 10 WORDS  ( MLFB of component: 20 bytes => 19 chars + 1 blank (0x20) )
-                                 0x0, # 1  WORD   ( RESERVED )
-                                 'V3', # 1  WORD   ( 'V' and first digit of version number )
+                                 0x0,  # 1  WORD   ( RESERVED )
+                                 'V3',  # 1  WORD   ( 'V' and first digit of version number )
                                  0x539)                     # 1  WORD   ( remaining digits of version number )
 
             ssl_resp_head = pack('!BBH',
-                                 0xff, # 1  BYTE   ( Data Error Code. 0xFF = OK )
-                                 0x09, # 1  BYTE   ( Data Type. 0x09 = Char/String )
-                                 len(ssl_resp_data))        # 1  WORD   ( Length of following data )
+                                 0xff,  # 1  BYTE   ( Data Error Code. 0xFF = OK )
+                                 0x09,  # 1  BYTE   ( Data Type. 0x09 = Char/String )
+                                 len(ssl_resp_data))  # 1  WORD   ( Length of following data )
 
-        elif (data_ssl_index == 7):  # 0x0007 - firmware identification
+        elif data_ssl_index == 7:  # 0x0007 - firmware identification
 
             ssl_index_description = 'Firmware identification'
 
             ssl_resp_data = pack('!HHHHH20sHHH',
-                                 17, # 1  WORD   ( ID )
-                                 data_ssl_index, # 1  WORD   ( Index )
-                                 28, # 1  WORD   ( Length of payload after element count )
-                                 0x01, # 1  WORD   ( 1 element follows )
-                                 data_ssl_index, # 1  WORD   ( Data Index )
-                                 0x0, # 10 WORDS  ( RESERVED )
-                                 0x0, # 1  WORD   ( RESERVED )
-                                 'V3', # 1  WORD   ( 'V' and first digit of version number )
-                                 0x53A)                     # 1  WORD   ( remaining digits of version number )
+                                 17,  # 1  WORD   ( ID )
+                                 data_ssl_index,  # 1  WORD   ( Index )
+                                 28,  # 1  WORD   ( Length of payload after element count )
+                                 0x01,  # 1  WORD   ( 1 element follows )
+                                 data_ssl_index,  # 1  WORD   ( Data Index )
+                                 0x0,  # 10 WORDS  ( RESERVED )
+                                 0x0,  # 1  WORD   ( RESERVED )
+                                 'V3',  # 1  WORD   ( 'V' and first digit of version number )
+                                 0x53A)  # 1  WORD   ( remaining digits of version number )
 
             ssl_resp_head = pack('!BBH',
-                                 0xff, # 1  BYTE   ( Data Error Code. 0xFF = OK )
-                                 0x09, # 1  BYTE   ( Data Type. 0x09 = Char/String )
-                                 len(ssl_resp_data))        # 1  WORD   ( Length of following data )
+                                 0xff,  # 1  BYTE   ( Data Error Code. 0xFF = OK )
+                                 0x09,  # 1  BYTE   ( Data Type. 0x09 = Char/String )
+                                 len(ssl_resp_data))  # 1  WORD   ( Length of following data )
 
         else:
             ssl_index_description = 'UNKNOWN / UNDEFINED / RESERVED {0}'.format(hex(data_ssl_index))
@@ -232,14 +232,14 @@ class S7(object):
             ssl_resp_head = ''
 
         ssl_resp_params = pack('!BBBBBBBB',
-                               0x00, # SSL DIAG
-                               0x01, # unknown
-                               0x12, # unknown
-                               0x08, # bytes following
-                               0x12, # unknown, maybe 0x11 + 1
-                               0x84, # function; response to 0x44
-                               0x01, # subfunction; readszl
-                               0x01)                # sequence ( = sequence + 1 )
+                               0x00,  # SSL DIAG
+                               0x01,  # unknown
+                               0x12,  # unknown
+                               0x08,  # bytes following
+                               0x12,  # unknown, maybe 0x11 + 1
+                               0x84,  # function; response to 0x44
+                               0x01,  # subfunction; readszl
+                               0x01)  # sequence ( = sequence + 1 )
         return ssl_index_description, ssl_resp_params, ssl_resp_head + ssl_resp_data
 
     # W#16#011C
@@ -249,74 +249,74 @@ class S7(object):
         current_ssl = S7.ssl_lists['W#16#xy1C']
         # initiate header for mass component block
         ssl_resp_data = pack('!HHHH',
-                             28, # 1  WORD   ( ID )
-                             data_ssl_index, # 1  WORD   ( Index )
-                             34, # 1  WORD   ( Length of payload after element count )
-                             0x08)                         # 1  WORD   ( 2 elements follow )
+                             28,  # 1  WORD   ( ID )
+                             data_ssl_index,  # 1  WORD   ( Index )
+                             34,  # 1  WORD   ( Length of payload after element count )
+                             0x08)  # 1  WORD   ( 2 elements follow )
 
         # craft module data 0x0001 - automation system name
         ssl_resp_data += pack('!H24s8s',
-                              0x01, # 1  WORD   ( Data Index )
-                              current_ssl['W#16#0001'], #TODO: PADDING
+                              0x01,  # 1  WORD   ( Data Index )
+                              current_ssl['W#16#0001'],  # TODO: PADDING
                               #'System Name             ', # 12 WORDS  ( Name of automation system, padded with (0x00) )
-                              '')                           # 4  WORDS  ( RESERVED )
+                              '')  # 4  WORDS  ( RESERVED )
 
         # craft module data 0x0002 - component name
         ssl_resp_data += pack('!H24s8s',
-                              0x02, # 1  WORD   ( Data Index )
-                              current_ssl['W#16#0002'], # 12 WORDS  ( Name of component, padded with (0x00) )
-                              '')                           # 4  WORDS  ( RESERVED )
+                              0x02,  # 1  WORD   ( Data Index )
+                              current_ssl['W#16#0002'],  # 12 WORDS  ( Name of component, padded with (0x00) )
+                              '')  # 4  WORDS  ( RESERVED )
 
         # craft module data 0x0003 - plant identification
         ssl_resp_data += pack('!H32s',
-                              0x03, # 1  WORD   ( Data Index )
-                              current_ssl['W#16#0003'],)   # 16 WORDS  ( Name of plant, padded with (0x00) )
+                              0x03,  # 1  WORD   ( Data Index )
+                              current_ssl['W#16#0003'],)  # 16 WORDS  ( Name of plant, padded with (0x00) )
 
         # craft module data 0x0004 - copyright
         ssl_resp_data += pack('!H26s6s',
-                              0x04, # 1  WORD   ( Data Index )
-                              current_ssl['W#16#0004'], # 13 WORDS  ( CONSTANT )
-                              '')                             # 3  WORDS  ( RESERVED )
+                              0x04,  # 1  WORD   ( Data Index )
+                              current_ssl['W#16#0004'],  # 13 WORDS  ( CONSTANT )
+                              '')  # 3  WORDS  ( RESERVED )
 
         # craft module data 0x0005 - module serial number
         ssl_resp_data += pack('!H24s8s',
-                              0x05, # 1  WORD   ( Data Index )
-                              current_ssl['W#16#0005'], # 12 WORDS  ( Unique Serial Number )
-                              '')                                   # 4  WORDS  ( RESERVED )
+                              0x05,  # 1  WORD   ( Data Index )
+                              current_ssl['W#16#0005'],  # 12 WORDS  ( Unique Serial Number )
+                              '')  # 4  WORDS  ( RESERVED )
 
         # craft module data 0x0007 - module type name
         ssl_resp_data += pack('!H32s',
-                              0x07, # 1  WORD   ( Data Index )
+                              0x07,  # 1  WORD   ( Data Index )
                               current_ssl['W#16#0007'],)   # 16 WORDS  ( CPU type name, padded wit (0x00) )
 
         # craft module data 0x000a - OEM ID of module
         ssl_resp_data += pack('!H20s6s2s4s',
-                              0x0a, # 1  WORD   ( Data Index )
-                              current_ssl['W#16#000A'], # 10 WORDS  ( OEM-Copyright Text, padded with (0x00) )
-                              '', # 3  WORDS  ( OEM Copyright Text padding to 26 characters )
-                              '', # 1  WORD   ( OEM ID provided by Siemens )
-                              '')                                   # 2  WORDS  ( OEM user defined ID )
+                              0x0a,  # 1  WORD   ( Data Index )
+                              current_ssl['W#16#000A'],  # 10 WORDS  ( OEM-Copyright Text, padded with (0x00) )
+                              '',  # 3  WORDS  ( OEM Copyright Text padding to 26 characters )
+                              '',  # 1  WORD   ( OEM ID provided by Siemens )
+                              '')  # 2  WORDS  ( OEM user defined ID )
 
         # craft module data 0x000b - location
         ssl_resp_data += pack('!H32s',
-                              0x0b, # 1  WORD   ( Data Index )
-                              current_ssl['W#16#000B'],)   # 16 WORDS  ( Location String, padded with (0x00) )
+                              0x0b,  # 1  WORD   ( Data Index )
+                              current_ssl['W#16#000B'],)  # 16 WORDS  ( Location String, padded with (0x00) )
 
         # craft leading response header
         ssl_resp_head = pack('!BBH',
-                             0xff, # 1  BYTE   ( Data Error Code. 0xFF = OK )
-                             0x09, # 1  BYTE   ( Data Type. 0x09 = Char/String )
-                             len(ssl_resp_data))        # 1  WORD   ( Length of following data )
+                             0xff,  # 1  BYTE   ( Data Error Code. 0xFF = OK )
+                             0x09,  # 1  BYTE   ( Data Type. 0x09 = Char/String )
+                             len(ssl_resp_data))  # 1  WORD   ( Length of following data )
 
         ssl_resp_packet = ssl_resp_head + ssl_resp_data
         ssl_resp_params = pack('!BBBBBBBB',
-                               0x00, # SSL DIAG
-                               0x01, # unknown
-                               0x12, # unknown
-                               0x08, # bytes following
-                               0x12, # unknown, maybe 0x11 + 1
-                               0x84, # function; response to 0x44
-                               0x01, # subfunction; readszl
-                               0x01)                # sequence ( = sequence + 1 )
+                               0x00,  # SSL DIAG
+                               0x01,  # unknown
+                               0x12,  # unknown
+                               0x08,  # bytes following
+                               0x12,  # unknown, maybe 0x11 + 1
+                               0x84,  # function; response to 0x44
+                               0x01,  # subfunction; readszl
+                               0x01)  # sequence ( = sequence + 1 )
 
         return '', ssl_resp_params, ssl_resp_packet
