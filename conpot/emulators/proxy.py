8163a3d1d3e494636eadeb92bc47733ec335e74d
Johnny Vestergaard <jkv@unixcluster.dk>
2014-05-19 11:28:56 +0200

    data file not needed anymore

--- conpot/emulators/proxy.py
+++ conpot/emulators/proxy.py
@@ -17,9 +17,6 @@
 
 import logging
 import select
-import os
-
-from datetime import datetime
 
 from gevent.socket import socket
 from gevent.server import StreamServer
@@ -58,11 +55,6 @@ class Proxy(object):
         logger.info('New connection from {0}:{1} on {2} proxy. ({3})'.format(address[0], address[1],
                                                                              self.proxy_id, session.id))
 
-        data_file = None
-        # to enable logging of raw socket data uncomment the following line
-        # and execute the commands: 'mkdir data; chown nobody:nobody data'
-        # data_file = open(os.path.join('data', 'Session-{0}'.format(session.id)), 'w', 0)
-
         proxy_socket = socket()
         proxy_socket.connect((self.proxy_host, self.proxy_port))
 
@@ -86,40 +78,32 @@ class Proxy(object):
                     else:
                         assert(False)
                 if s is proxy_socket:
-                    self.handle_out_data(data, sock, data_file, session)
+                    self.handle_out_data(data, sock, session)
                 elif s is sock:
-                    self.handle_in_data(data, proxy_socket, data_file, session)
+                    self.handle_in_data(data, proxy_socket, session)
                 else:
                     assert False
 
-        data_file.close()
         proxy_socket.close()
         sock.close()
 
-    def handle_in_data(self, data, sock, data_file, session):
+    def handle_in_data(self, data, sock, session):
         hex_data = data.encode('hex_codec')
         session.add_event({'raw_request': hex_data, 'raw_response': ''})
         logger.debug('Received {0} bytes from outside to proxied service: {1}'.format(len(data), hex_data))
-        if data_file:
-            self._dump_data(data_file, 'in', hex_data)
         if self.decoder:
             decoded = self.decoder.decode_in(data)
             session.add_event({'request': decoded, 'raw_response': ''})
         sock.send(data)
 
-    def handle_out_data(self, data, sock, data_file, session):
+    def handle_out_data(self, data, sock, session):
         hex_data = data.encode('hex_codec')
         session.add_event({'raw_request': '', 'raw_response': hex_data})
         logger.debug('Received {0} bytes from proxied service: {1}'.format(len(data), hex_data))
-        if data_file:
-            self._dump_data(data_file, 'out', hex_data)
         if self.decoder:
             decoded = self.decoder.decode_out(data)
             session.add_event({'request': '', 'raw_response': decoded})
         sock.send(data)
-
-    def _dump_data(self, file_handle, direction, hex_data):
-        file_handle.write('{0};{1};{2}\n'.format(datetime.utcnow().isoformat(), direction, hex_data))
 
     def _close(self, sockets):
         for s in sockets:
