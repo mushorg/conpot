17060d3364c5a8df59d18952af6e796c2f25bc72
Johnny Vestergaard <jkv@unixcluster.dk>
2014-05-25 20:46:51 +0200

    test case for ssl proxy

--- conpot/tests/test_proxy.py
+++ conpot/tests/test_proxy.py
@@ -16,22 +16,24 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import unittest
+import os
 
 import gevent
-import gevent.monkey
-
 from gevent.server import StreamServer
 from gevent.socket import socket
+from gevent.ssl import wrap_socket
 
+import conpot
 from conpot.emulators.proxy import Proxy
 
 gevent.monkey.patch_all()
 
-test_input = 'Hiya, this is a test'
+package_directory = os.path.dirname(os.path.abspath(conpot.__file__))
 
 
 class TestProxy(unittest.TestCase):
     def test_proxy(self):
+        self.test_input = 'Hiya, this is a test'
         mock_service = StreamServer(('127.0.0.1', 0), self.echo_server)
         gevent.spawn(mock_service.start)
         gevent.sleep(1)
@@ -43,16 +45,32 @@ class TestProxy(unittest.TestCase):
 
         s = socket()
         s.connect(('127.0.0.1', server.server_port))
-        test_input = 'Hiya, this is a test'
-        s.sendall(test_input)
-        received = s.recv(len(test_input))
-        self.assertEqual(test_input, received)
+        s.sendall(self.test_input)
+        received = s.recv(len(self.test_input))
+        self.assertEqual(self.test_input, received)
         mock_service.stop(1)
 
     def test_ssl_proxy(self):
-        # TODO
-        pass
+        self.test_input = 'Hiya, this is a test'
+        keyfile = os.path.join(package_directory, 'templates/example_ssl.key')
+        certfile = os.path.join(package_directory, 'templates/example_ssl.crt')
+
+        mock_service = StreamServer(('127.0.0.1', 0), self.echo_server, keyfile=keyfile, certfile=certfile)
+        gevent.spawn(mock_service.start)
+        gevent.sleep(1)
+
+        proxy = Proxy('proxy', '127.0.0.1', mock_service.server_port, keyfile=keyfile, certfile=certfile)
+        server = proxy.get_server('127.0.0.1', 0)
+        gevent.spawn(server.start)
+        gevent.sleep(1)
+
+        s = wrap_socket(socket(), keyfile, certfile)
+        s.connect(('127.0.0.1', server.server_port))
+        s.sendall(self.test_input)
+        received = s.recv(len(self.test_input))
+        self.assertEqual(self.test_input, received)
+        mock_service.stop(1)
 
     def echo_server(self, sock, address):
-        r = sock.recv(len(test_input))
+        r = sock.recv(len(self.test_input))
         sock.send(r)
