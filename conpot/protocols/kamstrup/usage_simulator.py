3c3209739ef54e37c57f8c27ab7bc245dfafe3aa
Johnny Vestergaard <jkv@unixcluster.dk>
2014-08-01 20:20:11 +0200

    usage simulator integrating back into the databus

--- conpot/protocols/kamstrup/usage_simulator.py
+++ conpot/protocols/kamstrup/usage_simulator.py
@@ -41,18 +41,87 @@ class UsageSimulator(object):
         databus.initialized.wait()
 
         # accumulated counter
-        self.energy_in = databus.get_value("register_13")
-        self.energy_out = databus.get_value("register_13")
-        # TODO: Overwrite databus values with function in this class
+        energy_in_register = 'register_13'
+        self.energy_in = databus.get_value(energy_in_register)
+        databus.set_value(energy_in_register, self.energy_in)
+        databus.set_value('register_1', self._get_energy_in_lowres())
 
-        # the following will serve be the values we diviate around
-        self.voltage[0] = databus.get_value("register_1054")
-        self.voltage[1] = databus.get_value("register_1055")
-        self.voltage[2] = databus.get_value("register_1056")
-        self.current[0] = databus.get_value("register_1076")
-        self.current[1] = databus.get_value("register_1077")
-        self.current[2] = databus.get_value("register_1078")
-        self.power[0] = databus.get_value("register_1080")
-        self.power[1] = databus.get_value("register_1081")
-        self.power[2] = databus.get_value("register_1082")
-        # TODO: Overwrite databus values with function in this class
+        energy_out_register = 'register_14'
+        self.energy_out = databus.get_value(energy_out_register)
+        databus.set_value(energy_out_register, self.energy_out)
+        databus.set_value('register_2', self._get_energy_out_lowres())
+
+        volt_1_register = 'register_1054'
+        self.voltage[0] = databus.get_value(volt_1_register)
+        databus.set_value(volt_1_register, self._get_voltage_1)
+
+        volt_2_register = 'register_1055'
+        self.voltage[1] = databus.get_value(volt_2_register)
+        databus.set_value(volt_2_register, self._get_voltage_2)
+
+        volt_3_register = 'register_1056'
+        self.voltage[2] = databus.get_value(volt_3_register)
+        databus.set_value(volt_3_register, self._get_voltage_3)
+
+        current_1_register = 'register_1076'
+        self.current[0] = databus.get_value(current_1_register)
+        databus.set_value(current_1_register, self._get_current_1)
+
+        current_2_register = 'register_1077'
+        self.current[1] = databus.get_value(current_2_register)
+        databus.set_value(current_2_register, self._get_current_2)
+
+        current_3_register = 'register_1078'
+        self.current[2] = databus.get_value(current_3_register)
+        databus.set_value(current_3_register, self._get_current_3)
+
+        power_1_register = 'register_1080'
+        self.power[0] = databus.get_value(power_1_register)
+        databus.set_value(power_1_register, self._get_power_1)
+
+        power_2_register = 'register_1081'
+        self.power[1] = databus.get_value(power_2_register)
+        databus.set_value(power_2_register, self._get_power_3)
+
+        power_3_register = 'register_1082'
+        self.power[2] = databus.get_value(power_3_register)
+        databus.set_value(power_3_register, self._get_power_3)
+
+    def _get_energy_in(self):
+        return self.energy_in
+
+    def _get_energy_out(self):
+        return self.energy_out
+
+    def _get_energy_in_lowres(self):
+        return int(self.energy_in)
+
+    def _get_energy_out_lowres(self):
+        return int(self.energy_out)
+
+    def _get_voltage_1(self):
+        return self.voltage[0]
+
+    def _get_voltage_2(self):
+        return self.voltage[1]
+
+    def _get_voltage_3(self):
+        return self.voltage[2]
+
+    def _get_current_1(self):
+        return self.current[0]
+
+    def _get_current_2(self):
+        return self.current[1]
+
+    def _get_current_3(self):
+        return self.current[2]
+
+    def _get_power_1(self):
+        return self.power[0]
+
+    def _get_power_2(self):
+        return self.power[1]
+
+    def _get_power_3(self):
+        return self.power[2]
