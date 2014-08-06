09ea5b062b23ce0b0a08639d9b6a1952710a6fe3
glaslos <lukas.rist@bluecoat.com>
2014-08-06 18:40:30 +0200

    STIX transform now using the python-stix module to generate the report.

--- conpot/core/loggers/stix_transform.py
+++ conpot/core/loggers/stix_transform.py
@@ -15,23 +15,40 @@
 # Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import uuid
-import os
 import json
 import ast
+import textwrap
+
+import stix
+import stix.utils
+from stix.core import STIXHeader, STIXPackage
+from stix.common import InformationSource
+from stix.common.vocabs import VocabString
+from stix.incident import Incident
+from stix.incident.time import Time as StixTime
+from stix.indicator import Indicator
+from stix.ttp import TTP, VictimTargeting
+from stix.extensions.identity.ciq_identity_3_0 import CIQIdentity3_0Instance, STIXCIQIdentity3_0, OrganisationInfo
+
+import cybox
+from cybox.core import Observable
+from cybox.objects.socket_address_object import SocketAddress
+from cybox.objects.address_object import Address
+from cybox.objects.port_object import Port
+from cybox.objects.network_connection_object import NetworkConnection
+from cybox.objects.artifact_object import Artifact, ZlibCompression, Base64Encoding
+from cybox.common import ToolInformationList, ToolInformation
+from cybox.common import Time as CyboxTime
 
 from datetime import datetime
-
-import jinja2
 
 import conpot
 
 
 class StixTransformer(object):
     def __init__(self, config, dom):
-        template_loader = jinja2.FileSystemLoader(searchpath=os.path.dirname(__file__))
-        template_env = jinja2.Environment(loader=template_loader)
         self.config = config._sections['taxii']
+        # This should not be hardcoded
         modbus_port = ast.literal_eval(dom.xpath('//conpot_template/protocols/modbus/@port')[0])
         snmp_port = ast.literal_eval(dom.xpath('//conpot_template/protocols/snmp/@port')[0])
         http_port = ast.literal_eval(dom.xpath('//conpot_template/protocols/http/@port')[0])
@@ -40,32 +57,89 @@ class StixTransformer(object):
                                          'http': http_port,
                                          's7comm': s7_port,
                                          'snmp': snmp_port}
-        self.template = template_env.get_template('stix_template.xml')
+
+    def _set_namespace(self, domain, name):
+        stix_namespace = {domain: name}
+        cybox_namespace = cybox.utils.nsparser.Namespace(domain, name)
+        stix.utils.idgen.set_id_namespace(stix_namespace)
+        cybox.utils.idgen.set_id_namespace(cybox_namespace)
+
+    def _add_header(self, stix_package, title, desc):
+        stix_header = STIXHeader()
+        stix_header.title = title
+        stix_header.description = desc
+        stix_header.information_source = InformationSource()
+        stix_header.information_source.time = CyboxTime()
+        stix_header.information_source.time.produced_time = datetime.now()
+        stix_package.stix_header = stix_header
 
     def transform(self, event):
-        data = {'package_id': str(uuid.uuid4()),
-                'namespace': 'ConPot',
-                'namespace_uri': 'http://conpot.org/stix-1',
-                'package_timestamp': datetime.utcnow().isoformat(),
-                'incident_id': event['session_id'],
-                'incident_timestamp': event['timestamp'].isoformat(),
-                'conpotlog_observable_id': str(uuid.uuid4()),
-                'network_observable_id': str(uuid.uuid4()),
-                'source_ip': event['remote'][0],
-                'source_port': event['remote'][1],
-                'l7_protocol': event['data_type'],
-                'conpot_version': conpot.__version__,
-                'session_log': json.dumps(event['data']),
-                'include_contact_info': self.config['include_contact_info'],
-                'contact_name': self.config['contact_name'],
-                'contact_mail': self.config['contact_email']}
+        self._set_namespace(self.config['contact_domain'], self.config['contact_name'])
+        stix_package = STIXPackage()
+        self._add_header(stix_package, "Unauthorized traffic to honeypot", "Describes one or more honeypot incidents")
 
-        if 'public_ip' in event:
-            data['destination_ip'] = event['public_ip']
+        incident = Incident(id_="%s:%s-%s" % (self.config['contact_name'], 'incident', event['session_id']))
+        initial_time = StixTime()
+        initial_time.initial_compromise = event['timestamp'].isoformat()
+        incident.time = initial_time
+        incident.title = "Conpot Event"
+        incident.short_description = "Traffic to Conpot ICS honeypot"
+        incident.add_category(VocabString(value='Scans/Probes/Attempted Access'))
 
-        if event['data_type'] in self.protocol_to_port_mapping:
-            data['destination_port'] = self.protocol_to_port_mapping[event['data_type']]
-        else:
-            raise Exception('No port mapping could be found for {0}'.format(event['data_type']))
+        tool_list = ToolInformationList()
+        tool_list.append(ToolInformation.from_dict({
+            'name': "Conpot",
+            'vendor': "Conpot Team",
+            'version': conpot.__version__,
+            'description': textwrap.dedent('Conpot is a low interactive server side Industrial Control Systems '
+                                           'honeypot designed to be easy to deploy, modify and extend.')
+        }))
+        incident.reporter = InformationSource(tools=tool_list)
 
-        return self.template.render(data)
+        incident.add_discovery_method("Monitoring Service")
+        incident.confidence = "High"
+
+        # Victim Targeting by Sector
+        ciq_identity = CIQIdentity3_0Instance()
+        #identity_spec = STIXCIQIdentity3_0()
+        #identity_spec.organisation_info = OrganisationInfo(industry_type="Electricity, Industrial Control Systems")
+        #ciq_identity.specification = identity_spec
+        ttp = TTP(title="Victim Targeting: Electricity Sector and Industrial Control System Sector")
+        ttp.victim_targeting = VictimTargeting()
+        ttp.victim_targeting.identity = ciq_identity
+
+        incident.leveraged_ttps.append(ttp)
+
+        indicator = Indicator(title="Conpot Event")
+        indicator.description = "Conpot network event"
+        indicator.confidence = "High"
+        source_port = Port.from_dict({'port_value': event['remote'][1], 'layer4_protocol': 'tcp'})
+        dest_port = Port.from_dict({'port_value': self.protocol_to_port_mapping[event['data_type']],
+                                    'layer4_protocol': 'tcp'})
+        source_ip = Address.from_dict({'address_value': event['remote'][0], 'category': Address.CAT_IPV4})
+        dest_ip = Address.from_dict({'address_value': event['public_ip'], 'category': Address.CAT_IPV4})
+        source_address = SocketAddress.from_dict({'ip_address': source_ip.to_dict(), 'port': source_port.to_dict()})
+        dest_address = SocketAddress.from_dict({'ip_address': dest_ip.to_dict(), 'port': dest_port.to_dict()})
+        network_connection = NetworkConnection.from_dict(
+            {'source_socket_address': source_address.to_dict(),
+             'destination_socket_address': dest_address.to_dict(),
+             'layer3_protocol': u"IPv4",
+             'layer4_protocol': u"TCP",
+             'layer7_protocol': event['data_type'],
+             'source_tcp_state': u"ESTABLISHED",
+             'destination_tcp_state': u"ESTABLISHED",
+             }
+        )
+        indicator.add_observable(Observable(network_connection))
+
+        artifact = Artifact()
+        artifact.data = json.dumps(event['data'])
+        artifact.packaging.append(ZlibCompression())
+        artifact.packaging.append(Base64Encoding())
+        indicator.add_observable(Observable(artifact))
+
+        incident.related_indicators.append(indicator)
+        stix_package.add_incident(incident)
+
+        stix_package_xml = stix_package.to_xml()
+        return stix_package_xml
