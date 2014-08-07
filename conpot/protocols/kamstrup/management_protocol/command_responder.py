e7914af99849a2e838c600d5391b23a3c753d40b
Andrea De Pasquale <andrea@de-pasquale.name>
2014-08-07 14:12:18 +0200

    Draft of !G* (get commands): help message, command message
    Help message is ready
    Command message needs to be formatted with values
    Values should be added to databus
    
    Strings are starting to get messy, needs refactoring

--- conpot/protocols/kamstrup/management_protocol/command_responder.py
+++ conpot/protocols/kamstrup/management_protocol/command_responder.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 
 class CommandResponder(object):
     def __init__(self, template):
-        self.cmd_not_found = (
+        self.command_not_found = (
             "\r\n"
             "? Command not found.\r\n"
             "Send 'H' for help.\r\n"
@@ -65,27 +65,78 @@ class CommandResponder(object):
             " Ie: H !SC\r\n"
         )
 
+        self.help_GC = (
+            "!GC: Get Config.\r\n"
+            "     Returns the module configuration.\r\n"
+        )
+
+        self.command_GC = (
+            "Device Name         : {}\r\n"
+            "Use DHCP            : {}\r\n"
+            "IP addr.            : {}\r\n"
+            "IP Subnet           : {}\r\n"
+            "Gateway addr.       : {}\r\n"
+            "Service server addr.: {}\r\n"
+            "Service server hostname.: {}\r\n"
+            "DNS Server No. 1: {}\r\n"
+            "DNS Server No. 2: {}\r\n"
+            "DNS Server No. 3: {}\r\n"
+            "MAC addr. (HEX)     : {}\r\n"
+            "Channel A device meterno.: {}\r\n"
+            "Channel B device meterno.: {}\r\n"
+            "Keep alive timer (flash setting): {} {}\r\n"
+            "Keep alive timer (current setting): {} {}\r\n"
+            "Has the module received acknowledge from the server: {}\r\n"
+            "KAP Server port: {}\r\n"
+            "KAP Local port: {}\r\n"
+            "Software watchdog: {} {}\r\n"
+        )
+
+        self.help_GV = (
+            "!GV: Software version.\r\n"
+            "     Returns the software revision of the module.\r\n"
+        )
+
+        self.command_GV = (
+            "\r\n"
+            "Software Version: {}\r\n"
+        )
+
     def respond(self, request):
-        stripped_request = request.strip().upper()
+        stripped_request = request.strip()
         split_request = stripped_request.split(" ", 1)
-        command = split_request[0]
+        command = split_request[0].upper()
 
         if len(command) > 3:
-            return self.cmd_not_found
+            return self.command_not_found
 
         if command.startswith("Q"):
             return  # quit
 
         if command.startswith("H"):
             if len(split_request) > 1:
-                return self.show_help(split_request[1:])
+                return self.show_help(split_request[1])
             return self.service_menu
 
-        # if stripped_request.startswith("!"):
-            # return "UNIMPLEMENTED"  # TODO
+        if command.startswith("!"):
+            return self.run_command(command)
 
         return ""  # default: idle
 
-    def show_help(self, params):
-        # TODO implement help for single commands
+    def show_help(self, command):
+        if command == "!GC":
+            return self.help_GC
+        if command == "!GV":
+            return self.help_GV
+
+        # TODO implement help
         return self.invalid_parameter
+
+    def run_command(self, command, params=None):
+        if command == "!GC":
+            return self.command_GC  # TODO format
+        if command == "!GV":
+            return self.command_GV  # TODO format
+
+        # TODO implement commands
+        return self.command_not_found
