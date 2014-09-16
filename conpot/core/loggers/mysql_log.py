37cff1a99bfc997ae0798109f898e9ca0f8dbf8c
Daniel Haslinger <creolis@@users.noreply.github.com>
2014-09-16 15:38:16 +0200

    further integration, autoinstall support and travis adaption

--- conpot/core/loggers/mysql_log.py
+++ conpot/core/loggers/mysql_log.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013  Lukas Rist <glaslos@gmail.com>
+# Copyright (C) 2014  Daniel creo Haslinger <creo-conpot@blackmesa.at>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -14,6 +14,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+import MySQLdb
 
 
 class MySQLlogger(object):
