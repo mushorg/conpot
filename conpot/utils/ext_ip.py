417521f19ae26a6f25cb6716574c7bee6e076432
Willem <git@canihavesome.coffee>
2016-05-05 03:01:15 +0200

    Remove telize as ip lookup service

--- conpot/utils/ext_ip.py
+++ conpot/utils/ext_ip.py
@@ -65,4 +65,4 @@ def get_ext_ip(config=None, urls=None):
 
 
 if __name__ == "__main__":
-    print get_ext_ip(urls=["http://www.telize.com/ip", "http://queryip.net/ip/", "http://ifconfig.me/ip"])
+    print get_ext_ip(urls=["http://queryip.net/ip/", "http://ifconfig.me/ip"])
